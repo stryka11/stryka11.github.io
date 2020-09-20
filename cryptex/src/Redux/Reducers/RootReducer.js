a38e210ffb0b687d164100c1cd5c08bb696ab782
= <=>
2020-09-20 21:32:12 +1000

    fixed reducer

--- cryptex/src/Redux/Reducers/RootReducer.js
+++ cryptex/src/Redux/Reducers/RootReducer.js
@@ -2,41 +2,31 @@ import { UPDATE_PIN } from "../Actions/RootActionTypes"
 
 const initialState = {
     userPin: Array.apply(null, Array(4)).map(function () {}),
-    passPin: [1,2,3,4], // hard code for now. read from config key later
+    passPin: ["1","2","3","4"], // hard code for now. read from config key later
     solved: false
 };
 
-function updateObjectInArray(array, action) {
-  return array.map((item, index) => {
-    if (index !== action.index) {
-      // This isn't the item we care about - keep it as-is
-      return item
-    }
-
-    // Otherwise, this is the one we want - return an updated value
-    return {
-      ...item,
-      ...action.item
-    }
-  })
+function arrayEquals(a, b) {
+  return Array.isArray(a) &&
+    Array.isArray(b) &&
+    a.length === b.length &&
+    a.every((val, index) => val === b[index]);
 }
+
 
 function rootReducer(state = initialState, action) {
     if (action.type == UPDATE_PIN) {
-        //update userPin value
-        console.log(action.payload);
 
         const index = action.payload.index
         const value = action.payload.value
 
         const updatedPin = [...state.userPin]
         updatedPin[index] = value
-        console.log(updatedPin);
-
 
         return {
             ...state,
             userPin : updatedPin,
+            solved : arrayEquals(state.passPin, updatedPin) //ugly. fix this later
             }
         }
 
